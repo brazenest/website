59ac0d0edd4ae34830f77b366a57100c83dc51b1
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-02-16 18:35:01 -0700

    Increase rounding in cta section

--- components/newton/layout/EmphasisSection.tsx
+++ components/newton/layout/EmphasisSection.tsx
@@ -9,7 +9,7 @@ export const EmphasisSection = ({
 	actions,
 }: EmphasisSectionProps) => {
 	return (
-		<section id={id} className="emphasis-section py-9.75 md:py-13 px-5 md:px-9.25 lg:px-8 bg-blue-50 dark:bg-blue-900 md:rounded-lg border-2 border-blue-300 dark:border-blue-800 text-center max-w-4xl lg:max-w-7xl mx-auto">
+		<section id={id} className="emphasis-section py-9.75 md:py-13 px-5 md:px-9.25 lg:px-8 bg-blue-50 dark:bg-blue-900 md:rounded-xl border-2 border-blue-300 dark:border-blue-800 text-center max-w-4xl lg:max-w-7xl mx-auto">
 			<div className="emphasis-section-content">
 				<span className="emphasis-section-inner pb-3 block">
 					<h2 className="emphasis-section-heading max-w-4xl lg:max-w-3xl mx-auto font-heading font-bold lg:font-semibold tracking-tight text-5xl leading-13.75 lg:text-6xl lg:leading-18.75 text-black dark:text-white mb-7 lg:mb-5">{heading}</h2>
