eea7240f921ba6066d3a87f4eb38f30565c7d511
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-03-06 12:53:37 -0700

    Add robots.txt and sitemap.xml generators

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+// app/robots.ts
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: {
+			userAgent: '*',
+			allow: '/',
+			disallow: ['/api/', '/admin/'],
+		},
+		sitemap: 'https://aldengillespy.com/sitemap.xml',
+	}
+}
