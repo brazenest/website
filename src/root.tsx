d6df4debca944cb50b10bf6fce04a4a7e1c9838c
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-03-17 15:14:26 -0600

    Add Inter as body font

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,14 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
 import {} from "~/types/ui";
 import "./global.css";
 
+import fontInterStyles from "@fontsource-variable/inter?inline";
+
 export default component$(() => {
+
+  useStyles$(fontInterStyles);
+
   return (
     <QwikCityProvider>
       <head>
